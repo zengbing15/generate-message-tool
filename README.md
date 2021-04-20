e9a863affcfb988cd359b322e7277bc2fee6e4c4
zengbing15 <zengb@cryptape.com>
2021-04-20 09:53:15 +0800

    chore: Commit index.js

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # generate-message-tool
 Generate `message` from a transaction.
+
+* Run a CKB Testnet node.
+* Install dependencies: `npm install`
+* Run generate-message-tool: `npm start`
---
+++ index.js
@@ -0,0 +1,54 @@
+
+const { RPC } = require("ckb-js-toolkit");
+const { common } = require('@ckb-lumos/common-scripts');
+const { Indexer } = require("@ckb-lumos/indexer");
+const {
+  TransactionSkeleton,
+  objectToTransactionSkeleton,
+} = require("@ckb-lumos/helpers");
+
+// use `config-manager` to get node info
+const { initializeConfig, getConfig } = require("@ckb-lumos/config-manager");
+const { List } = require("immutable");
+
+//TODO：for now we directly set up as testnet, will deal with this later. 
+process.env.LUMOS_CONFIG_NAME="AGGRON4";
+initializeConfig();
+
+const CKB_CONFIG = getConfig();
+
+// Get the node info and connect to the node.
+const CKB_RPC_URI = process.env.CKB_RPC_URI || "http://127.0.0.1:8114";
+const CKB_INDEXER_DATA = process.env.CKB_INDEXER_DATA || "./indexer-data";
+const indexer = new Indexer(CKB_RPC_URI, CKB_INDEXER_DATA);
+indexer.startForever();
+const rpc = new RPC(CKB_RPC_URI);
+
+
+/* Generate plain object of TransactionSkeleton */
+// TODO: Write a script to get the content of the transaction and call CKB JSON-RPC.
+// TODO: for now witness = {lock is 0, input_type is null, output_type is null},will deal with that input_type/output_type isn't null later.
+const obj = new Object();
+obj.cellProvider = {indexer};
+obj.cellDeps = List([{"out_point":{"tx_hash":"0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37","index":"0x0"},"dep_type":"dep_group"}]);
+obj.headerDeps = List([]);
+obj.inputs = List([{"cell_output":{"capacity":"0x5a07b819e92c","lock":{"code_hash":"0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8","hash_type":"type","args":"0xd6838b5e725103f2f1bfb58dbe64b930a857a263"}},"out_point":{"tx_hash":"0xb91a9893f65373d4a69baa43eb5dbbe55e54c1c6c87f404994a6f8622e36c139","index":"0x1"},"block_hash":"0x4780f397efc7fbe525d6fa0b2f700283c7a55e9d349ae76b1b2fef3ffe5ebb73","block_number":"0x17adbc","data":"0x"}]);
+obj.outputs = List([{"cell_output":{"capacity":"0xba43b7400","lock":{"code_hash":"0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8","hash_type":"type","args":"0x70e4d583965fa93e8329f09bd7ed7b600be7c766"}},"data":"0x"},{"cell_output":{"capacity":"0x59fc13de735c","lock":{"code_hash":"0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8","hash_type":"type","args":"0xd6838b5e725103f2f1bfb58dbe64b930a857a263"}},"data":"0x"}]);
+obj.witnesses = List(["0x55000000100000005500000055000000410000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"]);
+obj.fixedEntries = [];
+obj.signingEntries = [];
+obj.inputSinces = {};
+
+/* Convert js object to TransactionSkeleton */
+let txSkeleton = objectToTransactionSkeleton(obj);
+
+
+console.log(txSkeleton);
+console.log(typeof txSkeleton);
+console.log(JSON.stringify(txSkeleton));
+
+// Use `common.prepareSigningEntries` to generate `message`
+txSkeleton = common.prepareSigningEntries(txSkeleton //{ config: CKB_CONFIG }
+  );
+
+console.log(JSON.stringify(txSkeleton.signingEntries));
